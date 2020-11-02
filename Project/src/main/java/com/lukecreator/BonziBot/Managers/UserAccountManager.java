13d043c030c254b641becae498f84f63e8ac3a0c
7UKECREAT0R <43343249+7UKECREAT0R@users.noreply.github.com>
2020-11-02 14:50:57 -0500

    account manager

---
+++ Project/src/main/java/com/lukecreator/BonziBot/Managers/UserAccountManager.java
@@ -0,0 +1,47 @@
+package com.lukecreator.BonziBot.Managers;
+
+import java.util.HashMap;
+
+import com.lukecreator.BonziBot.InternalLogger;
+import com.lukecreator.BonziBot.Data.DataSerializer;
+import com.lukecreator.BonziBot.Data.IStorableData;
+import com.lukecreator.BonziBot.Data.UserAccount;
+import com.lukecreator.BonziBot.Legacy.UserAccountLegacyLoader;
+
+/*
+ * Handles all of the user accounts.
+ */
+public class UserAccountManager implements IStorableData {
+	
+	HashMap<Long, UserAccount> accounts;
+	
+	public UserAccountManager() {
+		accounts = new HashMap<Long, UserAccount>();
+	}
+	public UserAccount getUserAccount(long id) {
+		if(!accounts.containsKey(id))
+			accounts.put(id, new UserAccount());
+		return accounts.get(id);
+	}
+	public void setUserAccount(long id, UserAccount acct) {
+		accounts.put(id, acct);
+	}
+	
+	public void loadLegacy() {
+		accounts = UserAccountLegacyLoader.execute();
+		InternalLogger.print("Loaded legacy files.");
+	}
+	@Override
+	public void saveData() {
+		DataSerializer.writeObject(accounts, "modernaccounts");
+	}
+	@SuppressWarnings("unchecked")
+	@Override
+	public void loadData() {
+		Object o = DataSerializer.retrieveObject("modernaccounts");
+		if(o != null) {
+			accounts = (HashMap<Long, UserAccount>) o;
+		}
+	}
+	
+}
