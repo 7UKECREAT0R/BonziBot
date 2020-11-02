2bef06871cd6c382c35d02a62455c84db15c33ac
7UKECREAT0R <43343249+7UKECREAT0R@users.noreply.github.com>
2020-11-02 09:43:21 -0500

    Add files via upload

---
+++ Project/src/main/java/com/lukecreator/BonziBot/Managers/PrefixManager.java
@@ -0,0 +1,42 @@
+package com.lukecreator.BonziBot.Managers;
+
+import java.util.HashMap;
+
+import com.lukecreator.BonziBot.Constants;
+import com.lukecreator.BonziBot.Data.DataSerializer;
+import com.lukecreator.BonziBot.Data.IStorableData;
+
+/*
+ * Middleman for interacting with server prefixes.
+ */
+public class PrefixManager implements IStorableData {
+	
+	public static final String FILE = "prefixes";
+	
+	HashMap<Long, String> prefixData;
+	
+	public PrefixManager() {
+		prefixData = new HashMap<Long, String>();
+	}
+	public String getPrefix(long guild) {
+		if(prefixData.containsKey(guild))
+			return prefixData.get(guild);
+		else {
+			String p = Constants.DEFAULT_PREFIX;
+			prefixData.put(guild, p);
+			return p;
+		}
+	}
+
+	@Override
+	public void saveData() {
+		DataSerializer.writeObject(prefixData, FILE);
+	}
+	@SuppressWarnings("unchecked")
+	@Override
+	public void loadData() {
+		Object data = DataSerializer.retrieveObject(FILE);
+		if(data == null) return;
+		prefixData = (HashMap<Long, String>)data;
+	}
+}
