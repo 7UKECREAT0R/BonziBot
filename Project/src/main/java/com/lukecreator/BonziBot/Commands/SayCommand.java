a61cf17a2eed2941b4fdce091e740eac37ddb99e
7UKECREAT0R <43343249+7UKECREAT0R@users.noreply.github.com>
2020-11-02 11:48:54 -0500

    first command!

---
+++ Project/src/main/java/com/lukecreator/BonziBot/Commands/SayCommand.java
@@ -0,0 +1,26 @@
+package com.lukecreator.BonziBot.Commands;
+
+import com.lukecreator.BonziBot.CommandAPI.ACommand;
+import com.lukecreator.BonziBot.CommandAPI.ArgsComparison;
+import com.lukecreator.BonziBot.CommandAPI.CommandCategory;
+import com.lukecreator.BonziBot.CommandAPI.CommandExecutionInfo;
+
+public class SayCommand extends ACommand {
+	
+	public SayCommand() {
+		this.name = "Say";
+		this.description = "Make me say whatever you want!";
+		this.usage = "say <text>";
+		this.category = CommandCategory.FUN;
+		
+		this.usesArgs = true;
+		this.goalArgs = 1;
+		this.argsCheck = ArgsComparison.ANY_HIGHER;
+	}
+	
+	@Override
+	public void executeCommand(CommandExecutionInfo e) {
+		String toSay = e.getRemainder();
+		e.channel.sendMessage(toSay).queue();
+	}
+}
