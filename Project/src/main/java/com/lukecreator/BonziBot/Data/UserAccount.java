d4d34caf4d42bfab4c8470b6747b3378c2076bd8
7UKECREAT0R <43343249+7UKECREAT0R@users.noreply.github.com>
2020-11-13 23:24:22 -0500

    coins and xp methods

--- Project/src/main/java/com/lukecreator/BonziBot/Data/UserAccount.java
+++ Project/src/main/java/com/lukecreator/BonziBot/Data/UserAccount.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.lukecreator.BonziBot.BonziUtils;
+
 /*
  * Owned commands, coins, warns, etc...
  */
@@ -11,13 +13,47 @@ public class UserAccount implements Serializable {
 	
 	private static final long serialVersionUID = 1;
 	
-	public int coins = 0;
-	public int xp = 0;
+	private int coins = 0;
+	private int xp = 0;
 	
 	public List<ModernWarn> warns = new ArrayList<ModernWarn>();
 	
 	public boolean isPremium = false;
 	public List<PremiumItem> items = new ArrayList<PremiumItem>();
 	
+	// Getters
+	public int getCoins() {
+		return this.coins;
+	}
+	public int getXP() {
+		return this.xp;
+	}
+	public int getLevel() {
+		return BonziUtils.calculateLevel(this.xp);
+	}
 	
+	// Setters
+	/*
+	 * Returns if the user leveled up.
+	 */
+	public boolean incrementXP() {
+		return incrementXP(1);
+	}
+	public boolean incrementXP(int amount) {
+		int l = getLevel();
+		this.xp += amount;
+		return getLevel() != l;
+	}
+	public void setXP(int xp) {
+		this.xp = xp;
+	}
+	public void addCoins(int amount) {
+		this.coins += amount;
+	}
+	public void subCoins(int amount) {
+		this.coins -= amount;
+	}
+	public void setCoins(int coins) {
+		this.coins = coins;
+	}
 }
