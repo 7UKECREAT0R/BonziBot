e1583fe1b79f6a7bcf5ce65b487850c7364d8c10
7UKECREAT0R <43343249+7UKECREAT0R@users.noreply.github.com>
2020-12-21 11:50:07 -0500

    small changes in admin commands

--- Project/src/main/java/com/lukecreator/BonziBot/Commands/Admin/TestCommand.java
+++ Project/src/main/java/com/lukecreator/BonziBot/Commands/Admin/TestCommand.java
@@ -1,31 +1,30 @@
 package com.lukecreator.BonziBot.Commands.Admin;
 
-import com.lukecreator.BonziBot.CommandAPI.BooleanArg;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.lukecreator.BonziBot.CommandAPI.Command;
-import com.lukecreator.BonziBot.CommandAPI.CommandArgCollection;
 import com.lukecreator.BonziBot.CommandAPI.CommandCategory;
 import com.lukecreator.BonziBot.CommandAPI.CommandExecutionInfo;
+import com.lukecreator.BonziBot.Data.Modifier;
 
 public class TestCommand extends Command {
 
 	public TestCommand() {
 		this.name = "test";
 		this.description = "Tests a feature.";
-		this.args = new CommandArgCollection(
-				new BooleanArg("big chungus gamer"),
-				new BooleanArg("smol gamer"));
 		this.category = CommandCategory._HIDDEN;
 		this.adminOnly = true;
 	}
 	
 	@Override
 	public void executeCommand(CommandExecutionInfo e) {
-		/*GuiTestMenu menu = new GuiTestMenu();
-		BonziUtils.sendGuiFromExecutionInfo(e, menu);*/
-		
-		boolean bg = e.args.getBoolean("big chungus gamer");
-		boolean sg = e.args.getBoolean("smol gamer");
-		e.channel.sendMessage("Big chungus gamer: " + bg + "\nSmol gamer: " + sg).queue();
+		List<String> ls = new ArrayList<String>();
+		for(Modifier mod: e.modifiers) {
+			ls.add(mod.getDisplayName());
+		}
+		String s = String.join(", ", ls);
+		e.channel.sendMessage("Modifiers: " + s).queue();
 	}
 	
 }
